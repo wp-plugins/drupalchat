9a43b8684b7ee57e61563798aff935132a5594ad
darklrd <darklrd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-24 23:34:34 +0000

    Fixed jQuery requirement issue.
    git-svn-id: https://plugins.svn.wordpress.org/drupalchat/trunk@576885 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- drupalchat.php
+++ drupalchat.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package drupalchat
- * @version 1.0
+ * @version 1.0.1
  */
 /*
 Plugin Name: iFlyChat
 Plugin URI: http://wordpress.org/extend/plugins/drupalchat/
 Description: One on one chat 
 Author: Shashwat Srivastava, Shubham Gupta, Varun Kapoor - iFlyChat Team
-Version: 1.0
+Version: 1.0.1
 Author URI: https://iflychat.com/
 */
 
